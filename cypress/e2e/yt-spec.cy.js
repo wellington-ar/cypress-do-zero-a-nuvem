1f457c1b47ba14cd2293f68367ed84683a78e963
Wellington Araújo <wellingtonaraujonascimento7@gmail.com>
2026-03-28 13:39:57 -0400

    fix: delete youtube test

--- cypress/e2e/yt-spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://youtube.com/')
-
-    cy.get('[name="search_query"]').type('Manual do Mundo')
-    cy.get('.ytSearchboxComponentSearchButton').click()
-
-  })
-})
